8b292300a15d2617538290f5140c78bf4b68e2b4
energymaster22 <energymastervr@gmail.com>
2026-02-13 21:08:10 -0500

    started writing some helper functions

---
+++ YARG/yarghelpers.py
@@ -0,0 +1,40 @@
+def instnamechange(input):
+    #Swaps between the 2 formats of intsrument names
+    if input == "guitar5F":
+        return "Guitar"
+    if input == "bass5F":
+        return "Bass"
+    if input == "rhythm5F":
+        return "Rhythm"
+    if input == "drums":
+        return "Drums"
+    if input == "keys5F":
+        return "Keys"
+    if input == "keysPro":
+        return "Pro Keys"
+    if input == "vocals":
+        return "Vocals"
+    if input == "harmony2":
+        return "2 Part Harmony"
+    if input == "harmony3":
+        return "3 Part Harmony"
+    
+    
+    if input == "Guitar":
+        return "guitar5F"
+    if input == "Bass":
+        return "bass5f"
+    if input == "Rhythm":
+        return "rhythm5F"
+    if input == "Drums":
+        return "drums"
+    if input == "Keys":
+        return "keys5F"
+    if input == "Pro Keys":
+        return "keysPro"
+    if input == "Vocals":
+        return "vocals"
+    if input == "2 Part Harmony":
+        return "harmony2"
+    if input == "3 Part Harmony":
+        return "harmony3"
